397907355fed6f386c9f249e9a02ff193eba895d
Minhle13 <81848384+Minhle13@users.noreply.github.com>
2023-04-15 22:31:02 +0700

    NguyenLeMinh | remove | file

--- server/routes/AIRouter.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from controllers.AIController import AIController
-AIRouter = Blueprint('AIRouter', __name__)
-
-AIRouter.route('/', methods=['POST'])(AIController.ocr)
-
--- server/routes/Router.py
+++
@@ -1,8 +0,0 @@
-
-from routes.UserRouter import UserRouter
-from routes.AIRouter import AIRouter
-
-class Router:
-  def run(app):
-    app.register_blueprint(UserRouter, url_prefix='/users')
-    app.register_blueprint(AIRouter, url_prefix='/scan')
