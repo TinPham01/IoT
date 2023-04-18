33a46c638e6bd62a5e6acb760fe8cd54de88b694
Nguyenle23 <beenbin2308@gmail.com>
2023-04-18 10:32:26 +0700

    Remove

--- server/routes/AIRouter.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-from controllers.AIController import AIController
-AIRouter = Blueprint('AIRouter', __name__)
-
-AIRouter.route('/', methods=['POST'])(AIController.ocr)
-AIRouter.route('/face', methods=['GET'])(AIController.faceDetect)
-
